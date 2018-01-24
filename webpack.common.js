efc3538da6233caa2de7eeec9ec1694814eb3d5a
Joel Glemne <jglemne@jglemne-macbookair.roam.corp.google.com>
2018-01-24 21:33:37 +0100

    remove linux-incompatible file hash in webpack

--- webpack.common.js
+++ webpack.common.js
@@ -3,17 +3,17 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const WebpackGitHash = require('webpack-git-hash');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var fs = require('fs');
-var child_process = require('child_process');
-
-const hLength = 7
-var gitHash = child_process.execSync('git rev-parse --short=' + hLength + ' HEAD', { encoding: 'utf8' }).replace(/(\r\n|\n|\r)/gm,"");
+// var fs = require('fs');
+// var child_process = require('child_process');
+// 
+// const hLength = 7
+// var gitHash = child_process.execSync('git rev-parse --short=' + hLength + ' HEAD', { encoding: 'utf8' }).replace(/(\r\n|\n|\r)/gm,"");
 
 const config = {
     entry:  __dirname + '/assets/js/index.jsx',
     output: {
         path: __dirname + '/dist',
-        filename: 'bundle.[githash].js',
+        filename: 'bundle.[hash].js',
     },
     resolve: {
         extensions: [".js", ".jsx", ".css"]
@@ -47,15 +47,15 @@ const config = {
         new HtmlWebpackPlugin({template: 'assets/index.html'}),
         new CleanWebpackPlugin(['dist']),
         new ExtractTextPlugin({
-            filename: 'styles.' + gitHash + '.css'
+            filename: 'styles.[hash].css'
         }),
-        new WebpackGitHash({
-            callback: function(versionHash) {
-                var indexHtml = fs.readFileSync('./dist/index.html', 'utf8');
-                indexHtml = indexHtml.replace(/src="bundle\.\w+\.js/, 'src="bundle.' + versionHash + '.js').replace(/href="styles\.\w+\.css/, 'href="styles.' + versionHash + '.css');
-                fs.writeFileSync('./dist/index.html', indexHtml);
-            }
-        })
+        // new WebpackGitHash({
+        //     callback: function(versionHash) {
+        //         var indexHtml = fs.readFileSync('./dist/index.html', 'utf8');
+        //         indexHtml = indexHtml.replace(/src="bundle\.\w+\.js/, 'src="bundle.' + versionHash + '.js').replace(/href="styles\.\w+\.css/, 'href="styles.' + versionHash + '.css');
+        //         fs.writeFileSync('./dist/index.html', indexHtml);
+        //     }
+        // })
     ]
 };
 
